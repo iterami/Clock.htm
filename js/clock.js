be50bfb608314351ef587f29d0e0162db8b2f3c3
honzi <czechinthemail@gmail.com>
2017-02-26 01:17:56 -0800

    switched to common time.js usage

--- js/clock.js
+++ js/clock.js
@@ -7,53 +7,31 @@ function rotate_hand(id, percent){
 }
 
 function second(){
-    var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
+    var date = time_timestamp_to_date();
 
-    var date_display =
-      date.getFullYear()
-      + '-'
-      + two_digits(date.getMonth() + 1)
-      + '-'
-      + two_digits(date.getDate())
-      + ' '
-      + two_digits(hours)
-      + ':'
-      + two_digits(minutes)
-      + ':'
-      + two_digits(seconds)
-      + ' ('
-      + (date.getTimezoneOffset() / -60)
-      + ')';
-    document.getElementById('time').innerHTML = date_display;
+    document.getElementById('time').innerHTML = time_format_date({
+      'date': date,
+    });
 
-    if(hours > 11){
-        hours -= 12;
+    if(date['hour'] > 11){
+        date['hour'] -= 12;
     }
-    hours = hours / 12;
-    minutes = minutes / 60;
-    seconds = seconds / 60;
+    date['hour'] = date['hour'] / 12;
+    date['minute'] = date['minute'] / 60;
+    date['second'] = date['second'] / 60;
 
     rotate_hand(
       'hours-hand',
-      hours + minutes / 12
+      date['hour'] + date['minute'] / 12
     );
     rotate_hand(
       'minutes-hand',
-      minutes + seconds / 60
+      date['minute'] + date['second'] / 60
     );
     rotate_hand(
       'seconds-hand',
-      seconds
+      date['second']
     );
-}
-
-function two_digits(value){
-    return value.toString().length < 2
-      ? '0' + value
-      : value;
 }
 
 window.onload = function(){
