5c1e6a42d9ac8f833515d6f7e9dd6465c18d5082
honzi <czechinthemail@gmail.com>
2017-05-29 15:26:56 -0700

    added core_init(), repo_init(), and began global inputs

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,30 @@
 'use strict';
 
+function repo_init(){
+    second();
+
+    // Setup hands.
+    var hands = [
+      'hours-hand',
+      'minutes-hand',
+      'seconds-hand',
+    ];
+    for(var hand in hands){
+        document.getElementById(hands[hand]).style.background = '#' + core_random_hex();
+        document.getElementById(hands[hand]).style.height = '10px';
+        document.getElementById(hands[hand]).style.left = '50%';
+        document.getElementById(hands[hand]).style.position = 'fixed';
+        document.getElementById(hands[hand]).style.top = '50%';
+        document.getElementById(hands[hand]).style.transformOrigin = 'left';
+        document.getElementById(hands[hand]).style.width = (hand * 100 + 100) + 'px';
+    }
+
+    window.setInterval(
+      second,
+      1000
+    );
+}
+
 function rotate_hand(id, percent){
     document.getElementById(id).style.transform =
       'rotate(' + (360 * percent - 90) + 'deg)';
@@ -32,28 +57,3 @@ function second(){
       date['second']
     );
 }
-
-window.onload = function(){
-    second();
-
-    // Setup hands.
-    var hands = [
-      'hours-hand',
-      'minutes-hand',
-      'seconds-hand',
-    ];
-    for(var hand in hands){
-        document.getElementById(hands[hand]).style.background = '#' + core_random_hex();
-        document.getElementById(hands[hand]).style.height = '10px';
-        document.getElementById(hands[hand]).style.left = '50%';
-        document.getElementById(hands[hand]).style.position = 'fixed';
-        document.getElementById(hands[hand]).style.top = '50%';
-        document.getElementById(hands[hand]).style.transformOrigin = 'left';
-        document.getElementById(hands[hand]).style.width = (hand * 100 + 100) + 'px';
-    }
-
-    window.setInterval(
-      second,
-      1000
-    );
-};
