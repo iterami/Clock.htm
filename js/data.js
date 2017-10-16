bf2017e8a6f0b0b8e42a7e7e8045dbb2d31d1d3c
honzi <czechinthemail@gmail.com>
2017-10-15 18:03:06 -0700

    moved uncommon functions to data.js

---
+++ js/data.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function rotate_hand(id, percent){
+    document.getElementById(id).style.transform =
+      'rotate(' + (360 * percent - 90) + 'deg)';
+}
+
+function second(){
+    var date = time_timestamp_to_date();
+
+    document.getElementById('time').innerHTML = time_format_date({
+      'date': date,
+    });
+
+    if(date['hour'] > 11){
+        date['hour'] -= 12;
+    }
+    date['hour'] = date['hour'] / 12;
+    date['minute'] = date['minute'] / 60;
+    date['second'] = date['second'] / 60;
+
+    rotate_hand(
+      'hours-hand',
+      date['hour'] + date['minute'] / 12
+    );
+    rotate_hand(
+      'minutes-hand',
+      date['minute'] + date['second'] / 60
+    );
+    rotate_hand(
+      'seconds-hand',
+      date['second']
+    );
+}
