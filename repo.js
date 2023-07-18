60ba496bfb7ccf66a3a4d8783f6ac251ae96b14c
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 12:50:53 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -33,3 +33,36 @@ function repo_init(){
       'todo': second,
     });
 }
+
+function rotate_hand(id, percent){
+    document.getElementById(id).style.transform =
+      'rotate(' + (360 * percent - 90) + 'deg)';
+}
+
+function second(){
+    const date = timestamp_to_date();
+
+    document.getElementById('time').textContent = time_format({
+      'date': date,
+    });
+
+    if(date['hour'] > 11){
+        date['hour'] -= 12;
+    }
+    date['hour'] = date['hour'] / 12;
+    date['minute'] = date['minute'] / 60;
+    date['second'] = date['second'] / 60;
+
+    rotate_hand(
+      'hours-hand',
+      date['hour'] + date['minute'] / 12
+    );
+    rotate_hand(
+      'minutes-hand',
+      date['minute'] + date['second'] / 60
+    );
+    rotate_hand(
+      'seconds-hand',
+      date['second']
+    );
+}
