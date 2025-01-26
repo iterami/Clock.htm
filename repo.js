1da7aee2613f479720cbd2bd2457e9ea0e0cd4c3
Jan Hořava <czechinthemail@gmail.com>
2025-01-26 10:15:54 -0800

    added radius setting and moved style and colors to randomize()

--- repo.js
+++ repo.js
@@ -1,26 +1,6 @@
 'use strict';
 
-function repo_init(){
-    core_repo_init({
-      'events': {
-        'randomize': {
-          'onclick': core_repo_reset,
-        },
-      },
-      'info': '<button id=randomize type=button>Randomize Colors</button>',
-      'reset': update_colors,
-      'title': 'Clock.htm',
-      'ui-elements': [
-        'face',
-        'hours-hand',
-        'minutes-hand',
-        'seconds-hand',
-        'time',
-      ],
-    });
-
-    second();
-
+function randomize(){
     const hands = [
       'hours-hand',
       'minutes-hand',
@@ -33,20 +13,52 @@ function repo_init(){
         style.height = '10px';
         style.left = '50%';
         style.position = 'fixed';
-        style.top = '400px';
+        style.top = (core_storage_data['radius'] + 100) + 'px';
         style.transformOrigin = 'left';
-        style.width = (hand * 100 + 100) + 'px';
+        style.width = (hand * (core_storage_data['radius'] / 3) + (core_storage_data['radius'] / 3)) + 'px';
     }
 
     const style = core_elements['face'].style;
     style.backgroundColor = '#' + core_random_hex();
-    style.borderRadius = '300px';
-    style.height = '600px';
+    style.borderRadius = core_storage_data['radius'] + 'px';
+    style.height = (core_storage_data['radius'] * 2) + 'px';
     style.left = '50%';
-    style.marginLeft = '-300px';
+    style.marginLeft = -core_storage_data['radius'] + 'px';
     style.position = 'fixed';
     style.top = '105px';
-    style.width = '600px';
+    style.width = style.height;
+
+    for(const hand in hands){
+        core_elements[hands[hand]].style.backgroundColor = '#' + core_random_hex();
+    }
+    core_elements['face'].style.backgroundColor = '#' + core_random_hex();
+}
+
+function repo_init(){
+    core_repo_init({
+      'events': {
+        'randomize': {
+          'onclick': randomize,
+        },
+      },
+      'info': '<button id=randomize type=button>Randomize Colors</button>',
+      'reset': randomize,
+      'storage': {
+        'radius': 300,
+      },
+      'storage-menu': '<table><tr><td><input class=mini id=radius min=1 step=any type=number><td>px Radius</table>',
+      'title': 'Clock.htm',
+      'ui-elements': [
+        'face',
+        'hours-hand',
+        'minutes-hand',
+        'seconds-hand',
+        'time',
+      ],
+    });
+
+    second();
+    randomize();
 
     core_interval_modify({
       'id': 'clock',
@@ -89,17 +101,4 @@ function second(){
       'seconds-hand',
       date['second']
     );
-}
-
-function update_colors(){
-    const hands = [
-      'hours-hand',
-      'minutes-hand',
-      'seconds-hand',
-    ];
-    for(const hand in hands){
-        core_elements[hands[hand]].style.backgroundColor = '#' + core_random_hex();
-    }
-
-    core_elements['face'].style.backgroundColor = '#' + core_random_hex();
 }
